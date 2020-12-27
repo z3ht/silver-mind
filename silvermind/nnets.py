416f38f2b897bb9a8ab1d0781695085bef341d98
Andrew Darling <andr3w.darling@gmail.com>
2020-12-26 18:46:46 -0700

    Fix CNN

--- silvermind/nnets.py
+++ silvermind/nnets.py
@@ -1,24 +1,42 @@
-import keras
+from keras import models, layers
 
 class ChessCNN:
-	
-	def __init__(self):
+  
+  def __init__(self):
     self.model = None
   
   @staticmethod
   def _create_model():
-    pass
+    model = models.Sequential()
+    model.add(layers.Conv2D(16, (3, 3), activation="relu", input_shape=(5, 8, 8), data_format="channels_first", padding="same"))
+    model.add(layers.Conv2D(16, (3, 3), activation="relu",  data_format="channels_first", padding="same"))
+    model.add(layers.Conv2D(16, (3, 3), strides=(2,2), activation="relu",  data_format="channels_first", padding="same"))
+    model.add(layers.Conv2D(32, (3, 3), activation="relu", padding="same", data_format="channels_first"))
+    model.add(layers.Conv2D(32, (3, 3), activation="relu", padding="same", data_format="channels_first"))
+    model.add(layers.Conv2D(32, (3, 3), strides=(2,2), activation="relu", padding="same", data_format="channels_first"))
+    model.add(layers.Conv2D(64, (2, 2), activation="relu", padding="same", data_format="channels_first"))
+    model.add(layers.Conv2D(64, (2, 2), activation="relu", padding="same", data_format="channels_first"))
+    model.add(layers.Conv2D(64, (2, 2), strides=(2,2), activation="relu", padding="same", data_format="channels_first"))
+    model.add(layers.Conv2D(128, (1, 1), activation="relu", padding="same", data_format="channels_first"))
+    model.add(layers.Conv2D(128, (1, 1), activation="relu", padding="same", data_format="channels_first"))
+    model.add(layers.Conv2D(128, (1, 1), activation="relu", padding="same", data_format="channels_first"))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(128, activation="relu"))
+    model.add(layers.Dense(1, activation="sigmoid"))
+    model.compile(optimizer="Adam", loss="binary_crossentropy", metrics=["acc"])
+    return model
 
-  def train(self, X, y, test_split=False):
+  def train(self, X, y):
     """
     Train model with provided `X` and `y`
-
-    Optional Parameter `test_split` for analyzing test split accuracy and loss
     """
     model = ChessCNN._create_model()
-    model.fit(X, y, 
+    model.summary()
+    history = model.fit(X, y, batch_size=128, epochs=100, validation_split=0.2, shuffle=True)
 
     self.model = model
+
+    return history
 
   def save(self, save_path):
     """
@@ -32,7 +50,7 @@ class ChessCNN:
     """
     Load model from provided \`load_path\`
     """
-    self.model = keras.models.load_model(load_path)
+    self.model = models.load_model(load_path)
 
   def predict(self, serialized_board):
     if self.model is None:
