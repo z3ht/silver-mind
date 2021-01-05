e0b772073f65c4a13604056f92aa5c5b22781752
Andrew Darling <andr3w.darling@gmail.com>
2021-01-04 21:12:13 -0700

    Refactor DeepChess and implement random variable sampling

--- silvermind/nnets.py
+++ silvermind/nnets.py
@@ -1,6 +1,8 @@
 from keras import models, layers
 from keras import backend as K
 import numpy as np
+import os
+import random
 
 
 class ChessNet:
@@ -79,27 +81,53 @@ class DeepChess(ChessNet):
     def __init__(self):
         super().__init__()
         self.dbn_layers = [773, 100, 100, 100]
-        self.batch_size = 256
-        self.epochs = 2000
+        self.top = [100, 100, 2]
+        self.dbn_batch_size = 512
+        self.dbn_epochs = 200
+        self.batch_size = 512
+        self.epochs = 1000
 
-    def _create_dbn_model(self, X, y):
+    @staticmethod
+    def _random_samples(X, y, num_samples=800000):
+        white_inds, black_inds = np.where(y == 1), np.where(y == 0)
+        white_X, black_X = X[white_inds], X[black_inds]
+        white_y, black_y = y[white_inds], y[black_inds]
+        white_rows, black_rows = white_inds.shape[0], black_inds.shape[0]
+
+        Xs = []
+        ys = []
+        for _ in range(num_samples):
+            a = random.randint(0, white_rows - 1)
+            b = random.randint(0, black_rows - 1)
+            Xs.append([white_X[a], black_X[b]])
+            ys.append(np.stack(white_y[a], black_y[b], axis=1))
+
+        return np.array(Xs), np.array(ys)
+
+    def _create_base_model(self, X, y):
         model = models.Sequential()
         for i in range(1, len(self.dbn_layers)):
             train_model = models.Sequential()
             train_model.add(layers.Dense(self.dbn_layers[i], activation="relu", input_dim=self.dbn_layers[i - 1]))
             train_model.add(layers.Dense(self.dbn_layers[i - 1], activation="relu"))
             train_model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
-            train_model.fit(X, y, batch_size=self.batch_size, epochs=self.epochs)
+            train_model.fit(X, y, batch_size=self.dbn_batch_size, epochs=self.dbn_epochs)
 
             X = K.function([train_model.input], [train_model.layers[0].output])([X])[0]     # Retrieve next X
             weights = train_model.layers[0].get_weights()
 
             model.add(layers.Dense(self.dbn_layers[i], activation="relu", trainable=False, input_dim=self.dbn_layers[i-1]))
             model.layers[i-1].set_weights(weights)
+        model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
         return model
 
-    def _create_model(self, X, y):
-        base = self._create_dbn_model(X, y)
+    def _create_model(self, X, y, base_refresh=False, base_model_path="models/dbn.tf"):
+        base_model_path = __file__ + f"/../{base_model_path}"
+        if base_refresh or not os.path.isfile(base_model_path):
+            base = self._create_base_model(X, y)
+            base.save_model(save_path=base_model_path, save_format="tf")
+        else:
+            base = models.load_model(base_model_path)
 
         bot_left = layers.Input(shape=(self.dbn_layers[0],))
         bot_right = layers.Input(shape=(self.dbn_layers[0],))
@@ -107,20 +135,22 @@ class DeepChess(ChessNet):
         # Base layers are frozen so reuse is fine
         merged = layers.Concatenate([base(bot_left), base(bot_right)])
 
-        top2 = layers.Dense(100, activation="relu")(merged)
-        top3 = layers.Dense(100, activation="relu")(top2)
-        top4 = layers.Dense(2, activation="softmax")(top3)
+        top1 = layers.Dense(self.top[0], activation="relu", input_dim=self.dbn_layers[-1])(merged)
+        top2 = layers.Dense(self.top[1], activation="relu")(top1)
+        top3 = layers.Dense(self.top[2], activation="softmax")(top2)
 
-        model = models.Model(inputs=[bot_left, bot_right], outputs=top4)
+        model = models.Model(inputs=[bot_left, bot_right], outputs=top3)
         model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
         return model
 
-    def train(self, X, y):
-        model = self._create_model(X, y)
+    def train(self, X, y, base_refresh=False, base_model_path="models/dbn.tf", num_samples=800000):
+        X, y = self._random_samples(X=X, y=y, num_samples=num_samples)
 
+        model = self._create_model(X=X, y=y, base_refresh=base_refresh, base_model_path=base_model_path)
         model.summary()
-        history = model.fit(X, y, batch_size=512, epochs=200, validation_split=0.2, shuffle=True)
+
+        history = model.fit(X, y, use_multiprocessing=True, epochs=self.epochs, batch_size=self.batch_size)
 
         self.model = model
 
