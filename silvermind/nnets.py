8a0d3a364890e40fecf5750cb1247882d7434d39
Andrew Darling <andr3w.darling@gmail.com>
2021-01-05 13:59:29 -0700

    Fix regression metrics and optimize numpy

--- silvermind/nnets.py
+++ silvermind/nnets.py
@@ -80,7 +80,7 @@ class DeepChess(ChessNet):
 
     def __init__(self):
         super().__init__()
-        self.dbn_layers = [773, 100, 100, 100]
+        self.dbn_layers = [320, 100, 100, 100]
         self.top = [100, 100, 2]
         self.dbn_batch_size = 512
         self.dbn_epochs = 200
@@ -91,18 +91,23 @@ class DeepChess(ChessNet):
     def _random_samples(X, y, num_samples=800000):
         white_inds, black_inds = np.where(y == 1), np.where(y == 0)
         white_X, black_X = X[white_inds], X[black_inds]
-        white_y, black_y = y[white_inds], y[black_inds]
-        white_rows, black_rows = white_inds.shape[0], black_inds.shape[0]
+        white_rows, black_rows = len(white_inds), len(black_inds)
 
-        Xs = []
+        left, right = [], []
         ys = []
         for _ in range(num_samples):
             a = random.randint(0, white_rows - 1)
             b = random.randint(0, black_rows - 1)
-            Xs.append([white_X[a], black_X[b]])
-            ys.append(np.stack(white_y[a], black_y[b], axis=1))
+            c = random.random()
+            if c > 0.5:
+                sample = [white_X[a], black_X[b], [1, 0]]
+            else:
+                sample = [black_X[b], white_X[a], [0, 1]]
+            left.append(sample[0])
+            right.append(sample[1])
+            ys.append(sample[2])
 
-        return np.array(Xs), np.array(ys)
+        return [np.array(left), np.array(right)], np.array(ys)
 
     def _create_base_model(self, X):
         model = models.Sequential()
@@ -110,40 +115,41 @@ class DeepChess(ChessNet):
             train_model = models.Sequential()
             train_model.add(layers.Dense(self.dbn_layers[i], activation="relu", input_dim=self.dbn_layers[i - 1]))
             train_model.add(layers.Dense(self.dbn_layers[i - 1], activation="relu"))
-            train_model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
-            train_model.fit(X, X, batch_size=self.dbn_batch_size, epochs=self.dbn_epochs)
+            train_model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+            train_model.fit(X, X, batch_size=self.dbn_batch_size, epochs=self.dbn_epochs, verbose=False)
 
             X = K.function([train_model.input], [train_model.layers[0].output])([X])[0]     # Retrieve next X
             weights = train_model.layers[0].get_weights()
 
             model.add(layers.Dense(self.dbn_layers[i], activation="relu", trainable=False, input_dim=self.dbn_layers[i-1]))
             model.layers[i-1].set_weights(weights)
-        model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+        model.compile(optimizer='adam', loss='mse', metrics=['mae'])
         return model
 
     def _create_model(self, X, base_refresh=False, base_model_path="models/dbn.tf"):
-        base_model_path = __file__ + f"/../{base_model_path}"
         if base_refresh or not os.path.isfile(base_model_path):
             base = self._create_base_model(X)
-            base.save_model(save_path=base_model_path, save_format="tf")
+            # Keras does not support saving a model created w/ another model running
+            # base.save(base_model_path, save_format="tf")
         else:
             base = models.load_model(base_model_path)
 
         bot_left = layers.Input(shape=(self.dbn_layers[0],))
         bot_right = layers.Input(shape=(self.dbn_layers[0],))
 
-        merged = layers.Concatenate([base(bot_left), base(bot_right)])
+        merged = layers.Concatenate()([base(bot_left), base(bot_right)])
 
         top1 = layers.Dense(self.top[0], activation="relu", input_dim=self.dbn_layers[-1])(merged)
         top2 = layers.Dense(self.top[1], activation="relu")(top1)
         top3 = layers.Dense(self.top[2], activation="softmax")(top2)
 
         model = models.Model(inputs=[bot_left, bot_right], outputs=top3)
-        model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+        model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["mae"])
 
         return model
 
     def train(self, X, y, base_refresh=False, base_model_path="models/dbn.tf", num_samples=800000):
+        X = X.copy().reshape(X.shape[0], 8*8*5)
 
         model = self._create_model(X=X, base_refresh=base_refresh, base_model_path=base_model_path)
         model.summary()
