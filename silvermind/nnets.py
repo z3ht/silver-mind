7d8939e66f3bdb7ffd753610504b7cef4c75a097
Andrew Darling <andr3w.darling@gmail.com>
2021-01-05 11:25:41 -0700

    Fix autoencoder

--- silvermind/nnets.py
+++ silvermind/nnets.py
@@ -18,7 +18,7 @@ class ChessNet:
 
     def save(self, save_path):
         """
-    Save model to provided \`save_path\`
+    Save model to provided `save_path`
     """
         if self.model == None:
             raise RuntimeError("Model not trained; nothing to save")
@@ -104,14 +104,14 @@ class DeepChess(ChessNet):
 
         return np.array(Xs), np.array(ys)
 
-    def _create_base_model(self, X, y):
+    def _create_base_model(self, X):
         model = models.Sequential()
         for i in range(1, len(self.dbn_layers)):
             train_model = models.Sequential()
             train_model.add(layers.Dense(self.dbn_layers[i], activation="relu", input_dim=self.dbn_layers[i - 1]))
             train_model.add(layers.Dense(self.dbn_layers[i - 1], activation="relu"))
             train_model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
-            train_model.fit(X, y, batch_size=self.dbn_batch_size, epochs=self.dbn_epochs)
+            train_model.fit(X, X, batch_size=self.dbn_batch_size, epochs=self.dbn_epochs)
 
             X = K.function([train_model.input], [train_model.layers[0].output])([X])[0]     # Retrieve next X
             weights = train_model.layers[0].get_weights()
@@ -121,10 +121,10 @@ class DeepChess(ChessNet):
         model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
         return model
 
-    def _create_model(self, X, y, base_refresh=False, base_model_path="models/dbn.tf"):
+    def _create_model(self, X, base_refresh=False, base_model_path="models/dbn.tf"):
         base_model_path = __file__ + f"/../{base_model_path}"
         if base_refresh or not os.path.isfile(base_model_path):
-            base = self._create_base_model(X, y)
+            base = self._create_base_model(X)
             base.save_model(save_path=base_model_path, save_format="tf")
         else:
             base = models.load_model(base_model_path)
@@ -132,7 +132,6 @@ class DeepChess(ChessNet):
         bot_left = layers.Input(shape=(self.dbn_layers[0],))
         bot_right = layers.Input(shape=(self.dbn_layers[0],))
 
-        # Base layers are frozen so reuse is fine
         merged = layers.Concatenate([base(bot_left), base(bot_right)])
 
         top1 = layers.Dense(self.top[0], activation="relu", input_dim=self.dbn_layers[-1])(merged)
@@ -145,12 +144,13 @@ class DeepChess(ChessNet):
         return model
 
     def train(self, X, y, base_refresh=False, base_model_path="models/dbn.tf", num_samples=800000):
-        X, y = self._random_samples(X=X, y=y, num_samples=num_samples)
 
-        model = self._create_model(X=X, y=y, base_refresh=base_refresh, base_model_path=base_model_path)
+        model = self._create_model(X=X, base_refresh=base_refresh, base_model_path=base_model_path)
         model.summary()
 
-        history = model.fit(X, y, use_multiprocessing=True, epochs=self.epochs, batch_size=self.batch_size)
+        X, y = self._random_samples(X=X, y=y, num_samples=num_samples)
+
+        history = model.fit(X, y, use_multiprocessing=True, epochs=self.epochs, batch_size=self.batch_size, shuffle=True)
 
         self.model = model
 
